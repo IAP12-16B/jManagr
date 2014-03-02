054706702fb67f62d3e181f981901f3219d42de8
mnewmedia <maurer@screeneyes.com>
2014-03-02 22:58:54 +0100

    Tested sql2o

---
+++ src/ch/jManagr/DAL/Dada.java
@@ -0,0 +1,19 @@
+package ch.jManagr.DataAccessLayer;
+
+/**
+ * Created by Leyla on 02.03.14.
+ */
+public class Dada {
+
+    private int id;
+    private int no;
+    private String descr;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescr() {
+        return descr;
+    }
+}
---
+++ src/ch/jManagr/DAL/Test.java
@@ -0,0 +1,28 @@
+package ch.jManagr.DataAccessLayer;
+
+import org.sql2o.Sql2o;
+import java.util.List;
+
+class main {
+    public static void main(String[] args) {
+        Test t = new Test();
+        List<Dada> d = t.getAllTasks();
+        for(Dada k : d) {
+            System.out.println(k.getId() + k.getDescr());
+        }
+    }
+}
+
+public class Test {
+
+    private Sql2o sql2o;
+
+    public Test() {
+        this.sql2o   = new Sql2o("jdbc:mysql://localhost:3306/myDB", "root", "");
+    }
+
+    public List<Dada> getAllTasks(){
+        String sql = "SELECT id, no, descr FROM dada";
+        return this.sql2o.createQuery(sql).executeAndFetch(Dada.class);
+    }
+}
