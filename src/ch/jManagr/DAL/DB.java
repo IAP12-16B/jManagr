cd000555b827fb010020d8b40bb62253cd8de217
Kim Jeker <info@kije.ch>
2014-03-02 19:25:12 +0100

    created DB class (JDBC abstractor)

---
+++ src/ch/jManagr/DAL/DB.java
@@ -0,0 +1,36 @@
+package ch.jManagr.DAL;
+import java.sql.*;
+
+public class DB {
+    // static code block
+    static {
+        // load jdbc driver
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Get JDBC connection
+     * @return Connection   The Database connection
+     * @throws SQLException
+     */
+    private Connection connect() throws SQLException {
+        return DriverManager.getConnection(
+                String.format(
+                        "jdbc:mysql://%s:%s/%s?user=%s&password=%s",
+                        DBCredentials.getInstance().getHost(),
+                        DBCredentials.getInstance().getPort(),
+                        DBCredentials.getInstance().getDatabase(),
+                        DBCredentials.getInstance().getPassword()
+                )
+        );
+    }
+
+    public ResultSet select(String q) throws SQLException {
+        Statement statement = this.connect().createStatement();
+        return statement.executeQuery(q);
+    }
+}
