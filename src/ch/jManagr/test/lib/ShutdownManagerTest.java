763f80cbe9fefae4e0573191a53c3a161ae638aa
Kim Jeker <info@kije.ch>
2014-03-10 23:53:19 +0100

    Reworked Test

---
+++ src/ch/jManagr/test/lib/ShutdownManagerTest.java
@@ -0,0 +1,30 @@
+package ch.jManagr.test.lib;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by kije on 3/10/14.
+ */
+public class ShutdownManagerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetInstance() throws Exception {
+
+    }
+
+    @Test
+    public void testRegisterCallback() throws Exception {
+
+    }
+}
