a01214e92be25f1a18f25ef9dd6dedee7bdcfbd5
Kim Jeker <info@kije.ch>
2014-03-14 20:15:17 +0100

    Added Generic methode fetchAllOfType to DataAccessLayer-Interface

--- src/ch/jManagr/DAL/DataAccessLayer.java
+++ src/ch/jManagr/DAL/DataAccessLayer.java
@@ -1,5 +1,10 @@
 package ch.jManagr.DAL;
 
-public interface DataAccessLayer {
+import ch.jManagr.BO.BusinessObject;
 
+import java.util.ArrayList;
+
+public interface DataAccessLayer {
+    public void saveBusinessObject(BusinessObject);
+    public <T extends BusinessObject> ArrayList<T> fetchAllOfType(T bo);
 }
