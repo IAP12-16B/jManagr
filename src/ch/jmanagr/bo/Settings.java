043681f04bca2f57454a1807fcecc27665aedd59
Kim Jeker <info@kije.ch>
2014-04-01 19:30:29 +0200

    Implemented Settings

--- src/ch/jmanagr/bo/Settings.java
+++ src/ch/jmanagr/bo/Settings.java
@@ -1,10 +1,67 @@
 package ch.jmanagr.bo;
 
 
-public class Settings {
-    private String db_host = "localhost";
-    private String db_databse = "jManagr";
-    private String db_user = "root";
-    private String db_password = "";
-    private int db_port = 3306;
+public class Settings
+{
+	private String db_host;
+	private String db_database;
+	private String db_user;
+	private String db_password;
+	private int db_port;
+
+	/**
+	 * @return String the Host
+	 */
+	public String getHost()
+	{
+		return db_host;
+	}
+
+	/**
+	 * @param db_host
+	 */
+	public void setHost(String db_host)
+	{
+		this.db_host = db_host;
+	}
+
+	public String getDatabase()
+	{
+		return db_database;
+	}
+
+	public void setDatabase(String db_database)
+	{
+		this.db_database = db_database;
+	}
+
+	public String getUser()
+	{
+		return db_user;
+	}
+
+	public void setUser(String db_user)
+	{
+		this.db_user = db_user;
+	}
+
+	public String getPassword()
+	{
+		return db_password;
+	}
+
+	public void setPassword(String db_password)
+	{
+		this.db_password = db_password;
+	}
+
+	public int getPort()
+	{
+		return db_port;
+	}
+
+	public void setPort(int db_port)
+	{
+		this.db_port = db_port;
+	}
 }
--- src/ch/jmanagr/dal/Settings.java
+++ src/ch/jmanagr/dal/Settings.java
@@ -1,11 +1,57 @@
 package ch.jmanagr.dal;
 
+import ch.jmanagr.lib.STATUS_CODE;
+
 import java.util.prefs.Preferences;
 
-public class Settings {
-    private Preferences preferences;
+public class Settings
+{
+	private Preferences preferences;
+	private static volatile Settings instance;
 
-    public Settings() {
-        preferences = Preferences.userRoot().node(this.getClass().getName());
-    }
+	private Settings()
+	{
+		preferences = Preferences.userRoot().node(this.getClass().getName());
+	}
+
+	public STATUS_CODE put(ch.jmanagr.bo.Settings settings)
+	{
+		try {
+			preferences.put("DB_HOST", settings.getHost());
+			preferences.putInt("DB_PORT", settings.getPort());
+			preferences.put("DB_USER", settings.getUser());
+			preferences.put("DB_PASSWORD", settings.getPassword());
+			preferences.put("DB_DATABASE", settings.getDatabase());
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+			System.err.println(e.getLocalizedMessage());
+			return STATUS_CODE.FAIL;
+		}
+
+		return STATUS_CODE.OK;
+	}
+
+	public ch.jmanagr.bo.Settings get()
+	{
+		ch.jmanagr.bo.Settings settings = new ch.jmanagr.bo.Settings();
+		settings.setHost(preferences.get("DB_HOST", "localhost"));
+		settings.setPort(preferences.getInt("DB_PORT", 3306));
+		settings.setUser(preferences.get("DB_USER", "root"));
+		settings.setPassword(preferences.get("DB_PASSWORD", ""));
+		settings.setDatabase(preferences.get("DB_DATABASE", "jManagr"));
+
+		return settings;
+	}
+
+	public static Settings getInstance()
+	{
+		if (instance == null) {
+			synchronized (Settings.class) {
+				if (instance == null) {
+					instance = new Settings();
+				}
+			}
+		}
+		return instance;
+	}
 }
