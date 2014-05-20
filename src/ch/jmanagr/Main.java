82058cfa74950840c6cbee219bbafaed1014b43f
mnewmedia <maurer@screeneyes.com>
2014-05-20 18:42:52 +0200

    make ui working again

--- src/ch/jmanagr/Main.java
+++ src/ch/jmanagr/Main.java
@@ -1,6 +1,9 @@
 package ch.jmanagr;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application
@@ -9,14 +12,14 @@ public class Main extends Application
 	@Override
 	public void start(Stage primaryStage) throws Exception
 	{
-		/*Parent root = FXMLLoader.load(getClass().getResource("ui/main/main.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("ui/main/main.fxml"));
 		primaryStage.setTitle("jManagr");
 		primaryStage.setScene(new Scene(root));
 		primaryStage.setMinHeight(650);
 		primaryStage.setMinWidth(500);
 		primaryStage.setMaxHeight(700);
 
-		primaryStage.show();*/
+		primaryStage.show();
 	}
 
 
