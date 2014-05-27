d54f2559da73f02a0ea8e2b8db8bf2332cff0da5
Kim Jeker <info@kije.ch>
2014-05-27 23:50:11 +0200

    resolved merge conflict

--- src/ch/jmanagr/ui/tickets/TicketController.java
+++ src/ch/jmanagr/ui/tickets/TicketController.java
@@ -18,60 +18,70 @@ import java.util.ResourceBundle;
 
 public class TicketController implements Initializable
 {
-    private ObservableList<Ticket> ticketList;
-    private Tickets bl = Tickets.getInstance();
+	private ObservableList<Ticket> ticketList;
+	private Tickets bl = Tickets.getInstance();
 
-    @FXML private TableView<Ticket> ticketTable;
-    @FXML private TableColumn idCol;
-    @FXML private TableColumn<Ticket, String> nameCol;
+	@FXML
+	private TableView<Ticket> ticketTable;
+	@FXML
+	private TableColumn idCol;
+	@FXML
+	private TableColumn<Ticket, String> nameCol;
 
-    @FXML private TextField nameField;
+	@FXML
+	private TextField nameField;
 
-    // Fill Table with Data
-    public void initialize(URL location, ResourceBundle resources) {
+	// Fill Table with Data
+	public void initialize(URL location, ResourceBundle resources)
+	{
 
-        idCol.setCellValueFactory(new PropertyValueFactory("id"));
-        nameCol.setCellValueFactory(new PropertyValueFactory<Ticket, String>("name"));
+		idCol.setCellValueFactory(new PropertyValueFactory("id"));
+		nameCol.setCellValueFactory(new PropertyValueFactory<Ticket, String>("name"));
 
-        // makes nameCol to a textField
-        nameCol.setCellFactory(TextFieldTableCell.<Ticket>forTableColumn());
+		// makes nameCol to a textField
+		nameCol.setCellFactory(TextFieldTableCell.<Ticket>forTableColumn());
 
-        // sets the new Value after enterPressed in the ObserverList
-        nameCol.setOnEditCommit(
-                new EventHandler<TableColumn.CellEditEvent<Ticket, String>>() {
-                    public void handle(TableColumn.CellEditEvent<Ticket, String> t) {
+		// sets the new Value after enterPressed in the ObserverList
+		nameCol.setOnEditCommit(
+				new EventHandler<TableColumn.CellEditEvent<Ticket, String>>()
+				{
+					public void handle(TableColumn.CellEditEvent<Ticket, String> t)
+					{
 
-                        Ticket ticket = t.getTableView().getItems().get(t.getTablePosition().getRow()); //get changed object
-                        ticket.setName(t.getNewValue()); // set changed value
-                        bl.save(ticket);
-                        Logger.log("Updated in table ticket: " + t.getNewValue() + " " + ticket.getId());
-                    }
-                }
-        );
-        this.refresh();
-    }
+						Ticket ticket = t.getTableView().getItems().get(
+								t.getTablePosition()
+								 .getRow()
+						); //get changed object
+						ticket.setName(t.getNewValue()); // set changed value
+						bl.save(ticket);
+						Logger.log("Updated in table ticket: " + t.getNewValue() + " " + ticket.getId());
+					}
+				}
+		);
+		this.refresh();
+	}
 
-    public void refresh()
-    {
-        this.ticketList = bl.getAll();
-        this.ticketTable.setItems(this.ticketList);
-        Logger.log("Refreshed list!");
-    }
+	public void refresh()
+	{
+		this.ticketList = bl.getAll();
+		this.ticketTable.setItems(this.ticketList);
+		Logger.log("Refreshed list!");
+	}
 
-    public void newTicket()
-    {
-        //todo change view on newTicket()
-    }
+	public void newTicket()
+	{
+		//todo change view on newTicket()
+	}
 
-    // only archiv and only for admin?
-    public void deleteDep()
-    {
-        Ticket ticket = this.ticketTable.getSelectionModel().getSelectedItem();
-        if (ticket != null) {
-            Logger.log("Deleting ticket:" + ticket.getName() + " " + ticket.getId());
-            bl.delete(ticket);
-        } else {
-            Logger.log("Nothing selected to delete");
-        }
-    }
+	// only archiv and only for admin?
+	public void deleteDep()
+	{
+		Ticket ticket = this.ticketTable.getSelectionModel().getSelectedItem();
+		if (ticket != null) {
+			Logger.log("Deleting ticket:" + ticket.getName() + " " + ticket.getId());
+			bl.delete(ticket);
+		} else {
+			Logger.log("Nothing selected to delete");
+		}
+	}
 }
