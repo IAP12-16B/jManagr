0cb81ac82da85daf0f967b3e9fd9b8a61cab6659
mnewmedia <maurer@screeneyes.com>
2014-06-01 11:11:08 +0200

    added removeTabsByUser and some tabs with no content

--- src/ch/jmanagr/ui/main/MainController.java
+++ src/ch/jmanagr/ui/main/MainController.java
@@ -4,6 +4,7 @@ import ch.jmanagr.lib.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.AnchorPane;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,12 +15,17 @@ public class MainController implements Initializable
     @FXML private static AnchorPane ticketView;
     @FXML private static AnchorPane ticketDetailView;
     @FXML private static Tab tabTickets;
+    @FXML private static TabPane tabPane;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
         // can't make new instance because then the fxml property wouldnt be loaded
         // also cant make it singleton cause the constructor has to be public
+    }
+
+    public static void removeTabsByUserRole() {
+        //tabPane.getTabs().remove(tabTickets);
     }
 
     public static void changeTabContent(String $view) {
