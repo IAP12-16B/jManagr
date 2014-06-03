cd6496708e3a8179b0a675caf97a9065a5f05744
mnewmedia <maurer@screeneyes.com>
2014-06-03 10:09:00 +0200

    blub

--- src/ch/jmanagr/ui/users/UserDetailController.java
+++ src/ch/jmanagr/ui/users/UserDetailController.java
@@ -1,11 +1,16 @@
 package ch.jmanagr.ui.users;
 
 import ch.jmanagr.bl.Tickets;
+import ch.jmanagr.bl.Users;
 import ch.jmanagr.bo.*;
 import ch.jmanagr.lib.Logger;
 import ch.jmanagr.lib.TICKET_STATE;
+import ch.jmanagr.lib.USER_ROLE;
 import ch.jmanagr.ui.main.MainController;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.Date;
@@ -13,32 +18,32 @@ import java.util.ResourceBundle;
 
 public class UserDetailController implements Initializable
 {
-    private Tickets bl = Tickets.getInstance();
+    private Users bl = Users.getInstance();
+    @FXML private TextField lastnameFld;
+    @FXML private TextField firstnameFld;
+    @FXML private TextField usernameFld;
+    @FXML private TextField passwordFld;
+    @FXML private ComboBox departementCbox;
+    @FXML private ComboBox roleCbox;
+
     public void initialize(URL location, ResourceBundle resources) {
 
     }
 
     public void saveUser() {
-        // Todo @kije Errors maybe cause sql2o 1.5?
-        // Todo: @kije simplify this.... It should not be necessary to first create a bunch of dummy objects before
         // you can save the main object itself
-        Date d = new Date();
-        Resource r = BusinessObjects.getInstance(Resource.class, null);
-        Department de = BusinessObjects.getInstance(Department.class, null);
-        User u = BusinessObjects.getInstance(User.class, null); //Users.getInstance().getCurrentUser();
-        User agent = BusinessObjects.getInstance(User.class, null);
-        Ticket ticket = BusinessObjects.getInstance(Ticket.class, null);
-        ticket.setUser(u);
-        ticket.setAgent(agent);
-        ticket.setDepartment(de);
-        ticket.setResource(r);
-        ticket.setDate(d);
-        ticket.setStatus(TICKET_STATE.OPEN);
-        bl.save(ticket);
-        Logger.logln("Insertet new Ticket: ");
+        User user = new User();
+        user.setLastname(lastnameFld.getText());
+        user.setFirstname(firstnameFld.getText());
+        user.setUsername(usernameFld.getText());
+        user.setPassword(passwordFld.getText()); // Todo @kije had to make pw field in db bigger
+        //user.setDepartment();
+        user.setRole(USER_ROLE.USER);
+        bl.save(user);
+        Logger.logln("Insertet new User: ");
 
         //If it worked, return to list
-        MainController.changeTabContent("userDetail");
+        MainController.changeTabContent("users");
     }
 
     public void cancelUser() {
