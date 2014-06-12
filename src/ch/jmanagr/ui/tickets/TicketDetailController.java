d762fabe488d3f4e81620ddf85ced703dc979029
Kim Jeker <info@kije.ch>
2014-06-12 17:32:44 +0200

    Did some work for tickets

--- src/ch/jmanagr/ui/tickets/TicketDetailController.java
+++ src/ch/jmanagr/ui/tickets/TicketDetailController.java
@@ -3,12 +3,17 @@ package ch.jmanagr.ui.tickets;
 import ch.jmanagr.bl.DepartmentsBL;
 import ch.jmanagr.bl.TicketsBL;
 import ch.jmanagr.bl.UsersBL;
+import ch.jmanagr.bo.Department;
+import ch.jmanagr.bo.Ticket;
 import ch.jmanagr.bo.User;
 import ch.jmanagr.lib.Logger;
+import ch.jmanagr.lib.TICKET_STATE;
 import ch.jmanagr.ui.main.MainController;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.Date;
@@ -17,7 +22,13 @@ import java.util.ResourceBundle;
 public class TicketDetailController implements Initializable
 {
 	@FXML
-	private ComboBox departementCbox;
+	private ComboBox<Department> departementCbox;
+
+	@FXML
+	private TextArea descriptionFld;
+	@FXML
+	private TextField nameFld;
+
 	private TicketsBL bl;
 	private DepartmentsBL depBl;
 
@@ -38,19 +49,22 @@ public class TicketDetailController implements Initializable
 
 	public void saveTicket()
 	{
+		Ticket ticket = new Ticket();
+
 		Date d = new Date();
 		User u = UsersBL.getInstance().getCurrentUser();
-		/*Resource r = new Resource();
-		Department de = new Department();
+		Department de = departementCbox.getValue();
+		/*Resource r = new Resource(); // todo resource combo box
 		User agent = new User();
-		Ticket ticket = new Ticket();
-		ticket.setUser(u);
 		ticket.setAgent(agent);
+		ticket.setResource(r);*/
 		ticket.setDepartment(de);
-		ticket.setResource(r);
+		ticket.setUser(u);
 		ticket.setDate(d);
-		ticket.setStatus(TICKET_STATE.OPEN);
-		bl.save(ticket);*/
+		ticket.setStatus(TICKET_STATE.OPEN); // todo state combo box
+		ticket.setDescription(descriptionFld.getText());
+		ticket.setName(nameFld.getText());
+		bl.save(ticket);
 		Logger.logln("Insertet new Ticket: ");
 
 		//If it worked, return to list
