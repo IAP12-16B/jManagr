3723032f6eb00acec200220858339a58aa8fff5c
Kim Jeker <info@kije.ch>
2014-06-21 18:52:33 +0200

    Documented AbstractDAL

--- src/ch/jmanagr/dal/AbstractDAL.java
+++ src/ch/jmanagr/dal/AbstractDAL.java
@@ -16,6 +16,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
 
+/**
+ * Abstract class implementing all common operation for BusinessObjects
+ *
+ * @param <BusinessObjectType> the BusinessObject for which this Class should be responsible
+ */
 public abstract class AbstractDAL<BusinessObjectType extends BusinessObject<BusinessObjectType>>
 		implements DAL<BusinessObjectType>
 {
@@ -24,7 +29,9 @@ public abstract class AbstractDAL<BusinessObjectType extends BusinessObject<Busi
 
 	protected DB db;
 
-	// If this class is instantiated for the first time
+	/**
+	 * If the application runs the first time
+	 */
 	protected boolean firstUse = false;
 
 	protected AbstractDAL(Class<BusinessObjectType> typeClass) throws SQLException
@@ -42,41 +49,83 @@ public abstract class AbstractDAL<BusinessObjectType extends BusinessObject<Busi
 		}
 	}
 
+	/**
+	 * Create the table(s) necessary for this DAL
+	 *
+	 * @throws SQLException
+	 */
 	@Override
 	public void createTable() throws SQLException
 	{
 		this.db.createTableIfNotExists(typeClass);
 	}
 
+	/**
+	 * Deletes all entries in the table
+	 *
+	 * @throws SQLException
+	 */
 	@Override
 	public void clearTable() throws SQLException
 	{
 		this.db.clearTable(typeClass);
 	}
 
+	/**
+	 * Deletes the Table itself
+	 *
+	 * @throws SQLException
+	 */
 	@Override
 	public void dropTable() throws SQLException
 	{
 		this.db.dropTable(typeClass, false);
 	}
 
+	/**
+	 * Gets the BusinessObject by ID
+	 *
+	 * @param id the id of the BusinessObject
+	 *
+	 * @return The BusinessObject with the provided id or null, if not found
+	 *
+	 * @throws SQLException
+	 */
 	@Override
 	public BusinessObjectType fetchById(Integer id) throws SQLException
 	{
 		return dao.queryForId(id);
 	}
 
+	/**
+	 * Gets all BusinessObjects
+	 *
+	 * @return a list with all BusinessObjects
+	 *
+	 * @throws SQLException
+	 */
 	@Override
 	public List<BusinessObjectType> fetchAll() throws SQLException
 	{
 		return dao.queryForAll();
 	}
 
+	/**
+	 * Parametrized fetch Gets all BusinessObjects matching the parameters
+	 *
+	 * @param params Map of column = value, used in Where clause
+	 * @param limit  the maximum number of returned results. Values below 0 means no limit
+	 *
+	 * @return a list of all BusinessObjects matching the values provided by params
+	 *
+	 * @throws SQLException
+	 */
 	@Override
-	public List<BusinessObjectType> fetch(Map<String, Object> params) throws SQLException
+	public List<BusinessObjectType> fetch(Map<String, Object> params, int limit) throws SQLException
 	{
-		if (params.containsValue(null)) {
-			Where<BusinessObjectType, Integer> where = dao.queryBuilder().where();
+		if (params.containsValue(null) || limit >= 0) {
+			QueryBuilder<BusinessObjectType, Integer> queryBuilder = dao.queryBuilder();
+			Where<BusinessObjectType, Integer> where = queryBuilder.where();
 
 			Iterator<Map.Entry<String, Object>> entryIterator = params.entrySet().iterator();
 			while (entryIterator.hasNext()) {
@@ -93,12 +142,39 @@ public abstract class AbstractDAL<BusinessObjectType extends BusinessObject<Busi
 				}
 			}
 
-			return dao.query(where.prepare());
+			queryBuilder.setWhere(where);
+
+			if (limit >= 0) {
+				queryBuilder.limit((long) limit);
+			}
+
+			return dao.query(queryBuilder.prepare());
 		}
 
 		return dao.queryForFieldValues(params);
 	}
 
+	/**
+	 * Parametrized fetch without limit
+	 *
+	 * @see ch.jmanagr.dal.AbstractDAL#fetch(java.util.Map, int)
+	 */
+	@Override
+	public List<BusinessObjectType> fetch(Map<String, Object> params) throws SQLException
+	{
+		return this.fetch(params, -1);
+	}
+
+	/**
+	 * Parametrized fetch for one column = value pair
+	 *
+	 * @param fieldName the column name
+	 * @param value     the value to compare to
+	 *
+	 * @return a list of all BusinessObjects matching the criteria
+	 *
+	 * @throws SQLException
+	 */
 	@Override
 	public List<BusinessObjectType> fetch(String fieldName, Object value) throws SQLException
 	{
@@ -109,22 +185,54 @@ public abstract class AbstractDAL<BusinessObjectType extends BusinessObject<Busi
 		return dao.queryForEq(fieldName, value);
 	}
 
+	/**
+	 * Parametrized fetch for one column = value pair and a limit
+	 *
+	 * @param fieldName the column name
+	 * @param value     the value to compare to
+	 * @param limit     the maximum number of returned results. Values below 0 means no limit
+	 *
+	 * @return a list of all BusinessObjects matching the criteria
+	 *
+	 * @throws SQLException
+	 */
 	@Override
 	public List<BusinessObjectType> fetch(String fieldName, Object value, Integer limit) throws SQLException
 	{
 		QueryBuilder<BusinessObjectType, Integer> qb = dao.queryBuilder();
-		qb.limit(limit.longValue());
+		if (limit >= 0) {
+			qb.limit(limit.longValue());
+		}
+
 		qb.setWhere(qb.where().eq(fieldName, value));
 
 		return dao.query(qb.prepare());
 	}
 
+	/**
+	 * Check if the provided BusinessObject exists
+	 *
+	 * @param bo the BusinessObject to check
+	 *
+	 * @return true if it exists, false if not
+	 *
+	 * @throws SQLException
+	 */
 	@Override
 	public boolean exists(BusinessObjectType bo) throws SQLException
 	{
 		return dao.idExists(bo.getId());
 	}
 
+	/**
+	 * Deletes a BusinessObject
+	 *
+	 * @param bo the BusinessObject to delete
+	 *
+	 * @return Status code if it was successful
+	 *
+	 * @throws SQLException
+	 */
 	@Override
 	public STATUS_CODE delete(final BusinessObjectType bo) throws SQLException
 	{
@@ -144,6 +252,15 @@ public abstract class AbstractDAL<BusinessObjectType extends BusinessObject<Busi
 		);
 	}
 
+	/**
+	 * Deletes a collection of BusinessObject
+	 *
+	 * @param bos the BusinessObjects to delete
+	 *
+	 * @return Status code if it was successful
+	 *
+	 * @throws SQLException
+	 */
 	@Override
 	public STATUS_CODE delete(final Collection<BusinessObjectType> bos) throws SQLException
 	{
@@ -164,6 +281,15 @@ public abstract class AbstractDAL<BusinessObjectType extends BusinessObject<Busi
 		);
 	}
 
+	/**
+	 * Soft deletes (set deleted to 1) a BusinessObject
+	 *
+	 * @param bo the BusinessObject to soft-delete
+	 *
+	 * @return Status code if it was successful
+	 *
+	 * @throws SQLException
+	 */
 	public STATUS_CODE softDelete(final BusinessObjectType bo) throws SQLException
 	{
 		return TransactionManager.callInTransaction(
@@ -184,6 +310,15 @@ public abstract class AbstractDAL<BusinessObjectType extends BusinessObject<Busi
 		);
 	}
 
+	/**
+	 * Saves a BusinessObject to DB
+	 *
+	 * @param bo the BusinessObject to save
+	 *
+	 * @return Status code if it was successful
+	 *
+	 * @throws SQLException
+	 */
 	@Override
 	public STATUS_CODE save(final BusinessObjectType bo) throws SQLException
 	{
