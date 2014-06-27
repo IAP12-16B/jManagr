e1d2a328c0e96a5624157131e7547d995eb9e25b
mnewmedia <maurer@screeneyes.com>
2014-06-27 09:00:16 +0200

    make new userTickets view only to see own opened tickets..

---
+++ src/ch/jmanagr/ui/userTickets/userTicketsController.java
@@ -0,0 +1,119 @@
+package ch.jmanagr.ui.userTickets;
+
+import ch.jmanagr.bl.DepartmentsBL;
+import ch.jmanagr.bl.ResourcesBL;
+import ch.jmanagr.bl.TicketsBL;
+import ch.jmanagr.bl.UsersBL;
+import ch.jmanagr.bo.Department;
+import ch.jmanagr.bo.Resource;
+import ch.jmanagr.bo.Ticket;
+import ch.jmanagr.bo.User;
+import ch.jmanagr.exceptions.jManagrDBException;
+import ch.jmanagr.lib.LOG_LEVEL;
+import ch.jmanagr.lib.Logger;
+import ch.jmanagr.lib.TICKET_STATE;
+import ch.jmanagr.lib.USER_ROLE;
+import ch.jmanagr.ui.main.MainController;
+import ch.jmanagr.ui.tickets.TicketDetailController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+public class userTicketsController implements Initializable
+{
+	public static ObservableList<Ticket> ticketList;
+
+    private TicketsBL bl;
+    private UsersBL usersBL;
+    private DepartmentsBL depBL;
+    private ResourcesBL resBL;
+    private User currentUser;
+
+    // Controls for Table MyTickets
+	@FXML private static TableView<Ticket> myTicketTable;
+	@FXML private TableColumn myIdCol;
+	@FXML private TableColumn<Ticket, String> myNameCol;
+    @FXML private TableColumn<Ticket, Date> myDateCol;
+    @FXML private TableColumn<User, String> myAgentCol;
+    @FXML private TableColumn<Department, String> myDepartmentCol;
+    @FXML private TableColumn<Resource, String> myResourceCol;
+    @FXML private ComboBox<TICKET_STATE> myTicketsFilter;
+
+	public userTicketsController()
+	{
+		this.bl = TicketsBL.getInstance();
+        try {
+            this.usersBL = UsersBL.getInstance();
+        } catch (jManagrDBException e) {
+            Logger.log(LOG_LEVEL.ERROR, e);
+        }
+        this.resBL = ResourcesBL.getInstance();
+        this.depBL = DepartmentsBL.getInstance();
+        this.currentUser = this.usersBL.getCurrentUser();
+	}
+
+	public void initialize(URL location, ResourceBundle resources)
+	{
+        // --------------------- Table My Tickets ------------------------
+        // Fill Table with Data
+        myIdCol.setCellValueFactory(new PropertyValueFactory("id"));
+        myDepartmentCol.setCellValueFactory(new PropertyValueFactory<Department, String>("department"));
+        myNameCol.setCellValueFactory(new PropertyValueFactory<Ticket, String>("name"));
+        myDateCol.setCellValueFactory(new PropertyValueFactory<Ticket, Date>("date"));
+        //Todo make simpleProperty or dont show User here?
+        myAgentCol.setCellValueFactory(new PropertyValueFactory<User, String>("agent"));
+        myResourceCol.setCellValueFactory(new PropertyValueFactory<Resource, String>("resource"));
+
+        // Fill ticketsFilter Combobox
+        myTicketsFilter.getItems().addAll(TICKET_STATE.values());
+        myTicketsFilter.getSelectionModel().selectFirst();
+
+		this.refresh();
+	}
+
+	public void refresh()
+	{
+        ticketList = FXCollections.observableArrayList(bl.getAllByUser(currentUser, myTicketsFilter.getValue()));
+	}
+
+    public static void softRefresh() {
+        myTicketTable.getColumns().get(0).setVisible(false);
+        myTicketTable.getColumns().get(0).setVisible(true);
+    }
+
+	public void newTicket()
+	{
+        this.refreshComboBoxes();
+		MainController.changeTabContent("ticketDetail");
+	}
+
+	public void editTicket()
+	{
+        this.refreshComboBoxes();
+		Ticket selectedTicket = myTicketTable.getSelectionModel().getSelectedItem();
+		if (selectedTicket != null) {
+			TicketDetailController.fillTicket(selectedTicket);
+			MainController.changeTabContent("ticketDetail");
+		}
+	}
+
+    public void refreshComboBoxes() {
+        TicketDetailController.departementCbox.setItems(FXCollections.observableArrayList(depBL.getAll()));
+        TicketDetailController.agentCbox.setItems(FXCollections.observableArrayList(usersBL.getByUserRole(USER_ROLE.AGENT)));
+        TicketDetailController.resourceCbox.setItems(FXCollections.observableArrayList(resBL.getAll()));
+
+        TicketDetailController.departementCbox.getSelectionModel().selectFirst();
+        TicketDetailController.agentCbox.getSelectionModel().selectFirst();
+        TicketDetailController.resourceCbox.getSelectionModel().selectFirst();
+    }
+}
